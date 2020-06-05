ad4780eb04ff9b50eb945b86f047e52162cb1227
zhengaihua <you@example.com>
2020-06-05 19:07:51 +0800

    a

---
+++ ActivityTest/app/src/main/java/cn/imeixi/activitytest/ActivityCollector.java
@@ -0,0 +1,4 @@
+package cn.imeixi.activitytest;
+
+public class ActivityCollector {
+}
---
+++ ActivityTest/app/src/main/java/cn/imeixi/activitytest/BaseActivity.java
@@ -0,0 +1,6 @@
+package cn.imeixi.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivtiy extends AppCompatActivity {
+}
